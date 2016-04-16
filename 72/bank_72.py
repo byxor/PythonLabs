64495c1eb94f7fa90d6ccd5d456245e55fac2392
byxor <brandnewbyxor@gmail.com>
2016-04-16 16:57:40 +0100

    Solved bank_72.py

---
+++ 72/bank_72.py
@@ -0,0 +1,40 @@
+class BankAccount(object):
+	
+	def __init__(self, balance=0.0):
+		self.balance = balance
+	
+	def __str__(self):
+		return "Your current balance is: {:.2f} euro".format(self.balance)
+	
+	def deposit(self, amount):
+		self.balance += amount
+		
+	def withdraw(self, amount):
+		new_balance = self.balance - amount
+		if new_balance < 0:
+			print("Insufficient funds available")
+			return
+		self.balance = new_balance
+		
+def main():
+	b1 = BankAccount()
+	print(b1)
+	b1.withdraw(1)
+	b1.deposit(100)
+	b1.deposit(150)
+	print(b1)
+	b1.withdraw(50)
+	print(b1)
+	b1.deposit(20)
+	b1.withdraw(100)
+	print(b1)
+
+	b2 = BankAccount(1000)
+	b2.deposit(1)
+	b2.withdraw(2000)
+	print(b2)
+	b2.withdraw(1001)
+	print(b2)
+	
+if __name__ == "__main__":
+	main()
